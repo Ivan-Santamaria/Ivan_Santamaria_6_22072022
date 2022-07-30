229524d2a0dc5e63ba8728384f1d61c84c8a50c3
Ivan-Santamaria <81047384+Ivan-Santamaria@users.noreply.github.com>
2022-07-30 22:11:11 +0200

    Mise en place du middleware de validation

---
+++ back/middleware/validator.js
@@ -0,0 +1,15 @@
+const validator = require("validator");
+const passwordValidator = require("password-validator");
+
+module.exports = (req, res, next) => {
+  const passwordSchema = new passwordValidator()
+    .min(8, "Le mot de passe doit contenir au moins 8 caractères")
+    .uppercase(1, "Le mot de passe doit contenir au moins une majuscule")
+    .lowercase(1, "Le mot de passe doit contenir au moins une minuscule")
+    .digits(1, "Le mot de passe doit contenir au moins 1 chiffres")
+    .symbols(1, "Le mot de passe doit contenir au moins un caractère spécial")
+    .not()
+    .spaces(0, "Le mot de passe NE doit PAS contenir d'espaces");
+
+  console.log(passwordSchema.validate("Required", { list: true }));
+};
