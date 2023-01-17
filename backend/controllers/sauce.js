bbd5d71b2c4dfeb42c2520acba7182169f6c1937
Ivan-Santamaria <81047384+Ivan-Santamaria@users.noreply.github.com>
2023-01-17 19:14:01 +0100

    Modification sauces: remplacement img => dossier

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -26,35 +26,77 @@ exports.createSauce = (req, res, next) => {
     // Enregistre dans la db l'objet et renvoie une promesse
     .save()
     .then(() =>
-      res.status(201).json({ message: "Nouvelle sauce enregistrée !" })
+      res.status(201).json({
+        message:
+          "Nouvelle sauce enregistrée avec succès! Merci pour vôtre contribution.",
+      })
     )
-    .catch((error) => res.status(400).json({ error }));
+    .catch((error) =>
+      res.status(400).json({
+        error:
+          "Une erreur s'est produite empechant la sauvegarde le la sauce. Veuillez réessayer",
+      })
+    );
 };
 
-// Modifier la sauce
+// Modifierr la sauce
 exports.modifySauce = (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id }) // Methode pour trouver une sauce unique
+  // Recup sauce avec id
+  Sauce.findOne({ _id: req.params.id })
     .then((sauce) => {
       if (sauce.userId !== req.auth.userId) {
-        return res.status(404).json({ error: "Non autorisé" });
+        // Si L'id ne correspond pas => Non autorisé
+        return res.status(404).json({
+          error: "Vous n'avez pas la permission de modifier cette sauce!",
+        });
       } else {
-        const sauceObject = req.file
-          ? {
-              ...JSON.parse(req.body.sauce), // Récupération de toutes les infos sur l'objet
+        // Enregistrement ancienne imgUrl (si nouvelle image dans modify)
+        const oldUrl = sauce.imageUrl;
+        // Recuperation nom de l'image
+        const filename = sauce.imageUrl.split("/images/")[1];
+        // Suppression de l'image dans le dossier local
+        if (req.file) {
+          fs.unlink(`images/${filename}`, () => {
+            const sauceObject = {
+              ...JSON.parse(req.body.sauce),
               imageUrl: `${req.protocol}://${req.get("host")}/images/${
                 req.file.filename
               }`,
-            }
-          : { ...req.body };
-        Sauce.updateOne(
-          { _id: req.params.id },
-          { ...sauceObject, _id: req.params.id }
-        )
-          .then(() => res.status(200).json({ message: "Sauce modifiée !" }))
-          .catch((error) => res.status(400).json({ error }));
+            };
+            // Modification sauce
+            Sauce.updateOne(
+              { _id: req.params.id },
+              { ...sauceObject, _id: req.params.id }
+            )
+              .then(() =>
+                res.status(200).json({ message: "Image modifiée avec succès!" })
+              )
+              .catch((error) => res.status(400).json({ error }));
+          });
+        } else {
+          const newItem = req.body;
+          newItem.imageUrl = oldUrl;
+          Sauce.updateOne(
+            { _id: req.params.id, userId: req.body.userId },
+            { ...newItem, imageUrl: oldUrl, _id: req.params.id }
+          )
+            .then(() =>
+              res.status(200).json({ message: "Sauce modifiée avec succès!" })
+            )
+            .catch((error) =>
+              res.status(400).json({
+                error:
+                  "Une erreur inattendue s'est produite lors de la mise a jour de la sauce. Veuillez réessayez.",
+              })
+            );
+        }
       }
     })
-    .catch((error) => res.status(404).json({ error }));
+    .catch((error) =>
+      res.status(500).json({
+        error: "La sauce n'existe pas ou n'est pas disponible pour le moment. ",
+      })
+    );
 };
 
 // Supprimer la sauce
@@ -82,27 +124,44 @@ exports.deleteSauce = (req, res, next) => {
             { _id: req.params.id },
             { ...sauceObject, _id: req.params.id }
           )
-            .then(() => res.status(200).json({ message: "Sauce supprimée !" }))
+            .then(() =>
+              res.status(200).json({ message: "Sauce supprimée avec succès!" })
+            )
             .catch((error) => res.status(400).json({ error }));
         });
       }
     })
-    .catch((error) => res.status(404).json({ error }));
+    .catch((error) =>
+      res.status(404).json({
+        error: "La sauce n'existe pas ou n'est pas disponible pour le moment.",
+      })
+    );
 };
 
 // Recupérer une sauce (id)
 exports.getOneSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id }) // Methode pour trouver une sauce unique
     .then((sauce) => res.status(200).json(sauce))
-    .catch((error) => res.status(404).json({ error }));
+    .catch((error) =>
+      res.status(404).json({
+        error:
+          "La sauce n'existe pas pour le moment. Veuillez réessayer, n'hesitez pas à la créer vous même!",
+      })
+    );
 };
 
 // Récupération des sauces (toutes)
 exports.getAllSauces = (req, res, next) => {
   Sauce.find() // Methode renvoie un tableau contenant toutes les sauces dans la base de données
     .then((sauce) => res.status(200).json(sauce))
-    .catch((error) => res.status(400).json({ error }));
+    .catch((error) =>
+      res.status(404).json({
+        error:
+          "Aucune sauce n'est disponible pour le moment. Revenez plus tard ou créez en une! ",
+      })
+    );
 };
+
 
 // Appréciation, un seul like ou dislike par utilisateur
 exports.likeOrNot = (req, res, next) => {
