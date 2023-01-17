fc8d32a16ede209de22611e87f2fefab023a6169
Ivan-Santamaria <81047384+Ivan-Santamaria@users.noreply.github.com>
2023-01-17 20:20:26 +0100

    Modification commentaires

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,24 +2,20 @@
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 
-/* Verification authentification */
+// Verification de l'authentification
 module.exports = (req, res, next) => {
   try {
     // récupération du token
     // et split (diviser la chaîne de caratère en un tablaeau autour de l'espace qui se trouve entre notre mot clé bearer et token))
     const token = req.headers.authorization.split(" ")[1];
+    // Recupère le token depuis l'environement
     const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
     const userId = decodedToken.userId;
-    if (req.body.userId && req.body.userId !== userId) {
-      // Si le token et l'userdId ne correspondent pas => Erreur
-      throw "User ID Invalide";
-    } else {
-      // Si le token et l'userdId correspondent => Continuer
-      next();
-    }
-  } catch {
-    res.status(401).json({
-      error: new Error("Requête Invalide!"),
-    });
+    req.auth = {
+      userId: userId,
+    };
+    next();
+  } catch (error) {
+    res.status(401).json({ error });
   }
 };
